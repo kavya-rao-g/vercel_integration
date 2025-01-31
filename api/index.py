bb37e2a87294fafb57e957742e89e00f79abd8b9
Kavya G Rao <kavya.rao@gramener.com>
2025-01-31 16:30:22 +0530

    feat(api) : Updated index file for vercel

--- api/index.py
+++ api/index.py
@@ -19,14 +19,21 @@ def handler(request):
     names = request.args.getlist('name')
     
     marks_data = [{"name":"Qz8Y5uug","marks":18},{"name":"lKZelheI","marks":43},{"name":"MxNg3ItL","marks":32},{"name":"qaeqno","marks":20},{"name":"CMKPj","marks":8},{"name":"N3Ddd0T3","marks":81},{"name":"H2wSm19VC","marks":4},{"name":"q4LJFVI5K","marks":80},{"name":"8","marks":93},{"name":"VHlVPSTZ","marks":91},{"name":"ATo0CG","marks":70},{"name":"hRHf","marks":46},{"name":"NUbNnm36F","marks":79},{"name":"SfQ9jJN","marks":48},{"name":"O9j","marks":41}]
-    
+
     # Get the corresponding marks for the names
-    marks = [marks_data.get(name, None) for name in names]  # None for names not found
+    result = []
+    for name in names:
+        # Find the entry where the name matches
+        matching_entry = next((item for item in marks_data if item["name"] == name), None)
+        if matching_entry:
+            result.append(matching_entry["marks"])
+        else:
+            result.append(None)  # If name is not found, append None
     
     # Return the response as JSON
     return {
         'statusCode': 200,
-        'body': json.dumps({"marks": marks}),
+        'body': json.dumps({"marks": result}),
         'headers': {
             'Content-Type': 'application/json',
         }
