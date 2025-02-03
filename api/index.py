f9f053ca36c3ce9f304e175c3bc833ed3d5fd173
Kavya G Rao <kavya.rao@gramener.com>
2025-02-03 16:43:31 +0530

    feat(api) : Updated app index file for quick test

--- api/index.py
+++ api/index.py
@@ -15,7 +15,8 @@ class handler(BaseHTTPRequestHandler):
         # Extract names from the query parameters
         names = query_params.get("name", [])
 
-        marks_data = [{"name":"Qz8Y5uug","marks":18},{"name":"lKZelheI","marks":43},{"name":"MxNg3ItL","marks":32},{"name":"qaeqno","marks":20},{"name":"CMKPj","marks":8},{"name":"N3Ddd0T3","marks":81},{"name":"H2wSm19VC","marks":4},{"name":"q4LJFVI5K","marks":80},{"name":"8","marks":93},{"name":"VHlVPSTZ","marks":91},{"name":"ATo0CG","marks":70},{"name":"hRHf","marks":46},{"name":"NUbNnm36F","marks":79},{"name":"SfQ9jJN","marks":48},{"name":"O9j","marks":41}]
+
+        # marks_data = [{"name":"Qz8Y5uug","marks":18},{"name":"lKZelheI","marks":43},{"name":"MxNg3ItL","marks":32},{"name":"qaeqno","marks":20},{"name":"CMKPj","marks":8},{"name":"N3Ddd0T3","marks":81},{"name":"H2wSm19VC","marks":4},{"name":"q4LJFVI5K","marks":80},{"name":"8","marks":93},{"name":"VHlVPSTZ","marks":91},{"name":"ATo0CG","marks":70},{"name":"hRHf","marks":46},{"name":"NUbNnm36F","marks":79},{"name":"SfQ9jJN","marks":48},{"name":"O9j","marks":41}]
 
         # query_string = self.path.split('?')[1] if '?' in self.path else ''
         # params = dict(p.split('=') for p in query_string.split('&') if '=' in p)
@@ -26,19 +27,19 @@ class handler(BaseHTTPRequestHandler):
         #     names = [names]  # Convert single string to list
         
         # Find the corresponding marks for the names
-        result = []
-        for name in names:
-            matching_entry = next((item for item in marks_data if item["name"] == name), None)
-            if matching_entry:
-                # result.append(matching_entry["marks"])
-                result.append([18])
-            else:
-                result.append(None)
+        # result = []
+        # for name in names:
+        #     matching_entry = next((item for item in marks_data if item["name"] == name), None)
+        #     if matching_entry:
+        #         # result.append(matching_entry["marks"])
+        #         result.append(18)
+        #     else:
+        #         result.append(20)
 
         # Prepare JSON response
         return {
             'statusCode': 200,
-            'body': json.dumps({"marks": result}),
+            'body': json.dumps({"marks": names}),
             'headers': {
                 'Content-Type': 'application/json',
             }
